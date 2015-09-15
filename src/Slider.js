71331cb35ad22ff506a58ee740922cd8c4cbeab7
Ramana Venkata <idlike2dream@gmail.com>
2015-09-15 09:55:09 +0530

    refactor(slider): Observe the animation state of track
    Clicking the arrows mid-animation of track will not
    trigger slide change anymore.

--- src/Slider.js
+++ src/Slider.js
@@ -1,4 +1,6 @@
 import React, { Component, PropTypes, cloneElement, Children } from 'react';
+import { findDOMNode } from 'react-dom';
+import ReactTransitionEvents from 'react/lib/ReactTransitionEvents';
 
 class Slider extends Component {
   static propTypes = {
@@ -14,7 +16,8 @@ class Slider extends Component {
   constructor(props, context) {
     super(props, context);
     this.state = {
-      currentSlide: this.props.initialSlide
+      currentSlide: this.props.initialSlide,
+      animating: false
     };
   }
 
@@ -23,32 +26,47 @@ class Slider extends Component {
     const slides = this.props.children[1];
     const rightArrow = this.props.children[2];
     const slideCount = Children.count(slides.props.children);
-    const { currentSlide } = this.state;
+    const { currentSlide, animating } = this.state;
 
-    const handleLeftArrowClick = () => {
-      if (currentSlide > 0) {
-        this.setState({ currentSlide: currentSlide - 1 });
+    const slidesDOMNode = findDOMNode(this.refs.slides);
+    const trackDOMNode = slidesDOMNode.children[0];
+
+    const handleArrowClick = (delta) => {
+      if (animating) {
+        return;
       }
-    };
-    const handleRightArrowClick = () => {
-      if (currentSlide + 1 < slideCount) {
-        this.setState({ currentSlide: currentSlide + 1 });
+
+      const callback = () => {
+        this.setState({
+          animating: false
+        });
+        ReactTransitionEvents.removeEndEventListener(trackDOMNode, callback);
+      };
+
+      if ((delta < 0 && currentSlide > 0) || (delta > 0 && currentSlide + 1 < slideCount)) {
+        this.setState({
+          currentSlide: currentSlide + delta,
+          animating: true
+        }, () => {
+          ReactTransitionEvents.addEndEventListener(trackDOMNode, callback);
+        });
       }
     };
 
     const newLeftArrow = cloneElement(leftArrow, {
       key: 0,
-      handleClick: handleLeftArrowClick
+      handleClick: () => { handleArrowClick(-1); }
     });
 
     const newRightArrow = cloneElement(rightArrow, {
       key: 2,
-      handleClick: handleRightArrowClick
+      handleClick: () => { handleArrowClick(1); }
     });
 
     const newSlides = cloneElement(slides, {
       key: 1,
-      currentSlide
+      currentSlide,
+      ref: 'slides'
     });
 
     return (
